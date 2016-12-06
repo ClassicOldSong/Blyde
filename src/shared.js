5ed277e3687a962162e40d49cfc3bfa2e75b9ec1
Yukino Song <syqlds@126.com>
2016-12-07 05:22:05 +0800

    Improved performance

--- src/shared.js
+++ src/shared.js
@@ -9,12 +9,10 @@ const methods = {
 const $node = class {
 	constructor(node) {
 		this.$el = node
-		let $nodeMethods = {}
 		for (let i in methods.node) {
-			if (methods.node[i] instanceof Function) $nodeMethods[i] = methods.node[i].bind(node)
-			else $nodeMethods[i] = methods.node[i]
+			if (methods.node[i] instanceof Function) this[i] = methods.node[i].bind(node)
+			else this[i] = methods.node[i]
 		}
-		Object.assign(this, $nodeMethods)
 		if (!node.$id) Object.defineProperty(node, '$id', {value: Math.floor(Math.random() * Math.pow(10, 16)).toString(36)})
 	}
 }
@@ -22,12 +20,10 @@ const $nodeList = class {
 	constructor(list) {
 		this.$list = []
 		for (let i = 0; i < list.length; i++) this.$list.push(list[i].$)
-		let $listMethods = {}
 		for (let i in methods.list) {
-			if (methods.list[i] instanceof Function) $listMethods[i] = methods.list[i].bind(this.$list)
-			else $listMethods[i] = methods.node[i]
+			if (methods.list[i] instanceof Function) this[i] = methods.list[i].bind(this.$list)
+			else this[i] = methods.node[i]
 		}
-		Object.assign(this, $listMethods)
 	}
 }
 
